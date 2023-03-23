dde5af61e430d6a2904df31510e7390caa847282
vishnusaimanchineella <imvishnusai@gmail.com>
2023-03-23 19:04:07 +0530

    Implemented DAO Pattern and N-tier Architecture with service layer

---
+++ services/StudentEntryService.java
@@ -0,0 +1,13 @@
+package services;
+
+import logic.models.Student;
+
+public interface StudentEntryService {
+    void add(Student student);
+
+    void remove(Student student);
+
+    Student[] getAll();
+
+    Student get(String id);
+}
